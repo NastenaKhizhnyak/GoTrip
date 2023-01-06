9f624ed67327215ec2700ac2285ac58c47f790f3
Anastasiia Khyzhniak <76616897+NastenaKhizhnyak@users.noreply.github.com>
2023-01-06 15:10:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+//Carusell 
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        center:true,
+        loop:true,
+        margin:30,
+        startPosition:1,
+        items:3,
+    });
+  });
+
+
+
+  const owl = $('.owl-carousel');
+    owl.owlCarousel({
+        center:true,
+        loop:true,
+        margin:30,
+        startPosition:1,
+        items:3,
+    });
+    // Go to the previous item
+    $('.slider__btn--prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel');
+    })
+    // Go to the next item
+    $('.slider__btn--next').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    
+
+    // Burger
+
+    const navBtn= document.querySelector('.nav__toggle');
+    const nav = document.querySelector('.nav');
+    const menuIcon= document.querySelector('.menu-icon');
+
+
+    navBtn.onclick = function(){
+        nav.classList.toggle('nav--mobile');
+        menuIcon.classList.toggle('menu-icon-active');
+    }
